dc55bf8501f3aa092915b6b00c6724dbf9d7ce37
Hong Ha Vo <havh0108@gmail.com>
2017-03-08 19:43:59 +0700

    add profile material design

--- app/src/main/java/com/auto/jarvis/libraryicognite/activities/ProfileActivity.java
+++ app/src/main/java/com/auto/jarvis/libraryicognite/activities/ProfileActivity.java
@@ -4,17 +4,36 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.auto.jarvis.libraryicognite.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import static android.content.Intent.FLAG_ACTIVITY_PREVIOUS_IS_TOP;
 
 public class ProfileActivity extends AppCompatActivity {
 
+//    @BindView(R.id.tvUserFullName)
+//    TextView tvUserFullName;
+//
+//    @BindView(R.id.tvUserId)
+//    TextView tvUserId;
+//
+//    @BindView(R.id.tvPassword)
+//    TextView tvPassword;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
     }
 
     public static Intent getIntentNewTask(Context context) {
