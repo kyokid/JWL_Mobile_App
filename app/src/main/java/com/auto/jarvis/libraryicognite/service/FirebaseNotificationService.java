0d6fcfcedc9595b4f654f9b4c9afeeae41b42ec2
Hong Ha Vo <havh0108@gmail.com>
2017-03-21 08:08:40 +0700

    push notification when run thread checkdeadline on server

--- app/src/main/java/com/auto/jarvis/libraryicognite/service/FirebaseNotificationService.java
+++ app/src/main/java/com/auto/jarvis/libraryicognite/service/FirebaseNotificationService.java
@@ -1,11 +1,19 @@
 package com.auto.jarvis.libraryicognite.service;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.auto.jarvis.libraryicognite.R;
 import com.auto.jarvis.libraryicognite.Utils.Constant;
+import com.auto.jarvis.libraryicognite.activities.BorrowCartActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
@@ -14,14 +22,43 @@ import com.google.firebase.messaging.RemoteMessage;
  * Created by thiendn on 22/02/2017.
  */
 
-public class FirebaseNotificationService extends FirebaseMessagingService{
+public class FirebaseNotificationService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         //Toast.makeText(getApplicationContext(), "Receive message: " + remoteMessage.getFrom(), Toast.LENGTH_SHORT).show();
-        Log.d("FirebaseNoti: " , remoteMessage.getNotification().getBody());
-        Intent pushNotification = new Intent(Constant.PUSH_NOTIFICATION);
-        pushNotification.putExtra("message", remoteMessage.getNotification().getBody());
-        LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+        Log.d("FirebaseNoti: ", remoteMessage.getNotification().getBody());
+        String title = remoteMessage.getNotification().getTitle();
+        if (title != null && title.equals("Remaining day")) {
+            sendNotification(remoteMessage);
+        } else {
+            Intent pushNotification = new Intent(Constant.PUSH_NOTIFICATION);
+            pushNotification.putExtra("message", remoteMessage.getNotification().getBody());
+            LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
+        }
+    }
+
+    private void sendNotification(RemoteMessage remoteMessage) {
+        Intent intent = new Intent(this, BorrowCartActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+//                .setSmallIcon(R.drawable.ic_movie_black_24dp)
+                .setAutoCancel(true)
+                .setSmallIcon(R.mipmap.ic_launcher_ver2)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(remoteMessage.getNotification().getTitle())
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
     }
 }
