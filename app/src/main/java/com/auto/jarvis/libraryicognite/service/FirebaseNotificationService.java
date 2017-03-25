31f0d531612639725912c6f5dee99097c715c0bc
Hong Ha Vo <havh0108@gmail.com>
2017-03-25 08:02:40 +0700

    decode message and bigstyle text for notification, background and foreground

--- app/src/main/java/com/auto/jarvis/libraryicognite/service/FirebaseNotificationService.java
+++ app/src/main/java/com/auto/jarvis/libraryicognite/service/FirebaseNotificationService.java
@@ -17,6 +17,9 @@ import com.auto.jarvis.libraryicognite.activities.BorrowCartActivity;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 
 /**
  * Created by thiendn on 22/02/2017.
@@ -27,24 +30,31 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
         //Toast.makeText(getApplicationContext(), "Receive message: " + remoteMessage.getFrom(), Toast.LENGTH_SHORT).show();
-        Log.d("FirebaseNoti: ", remoteMessage.getNotification().getBody());
-        String title = remoteMessage.getNotification().getTitle();
-            if (title != null && title.equals("Remaining day")) {
+//        Log.d("FirebaseNoti: ", remoteMessage.getNotification().getBody());
+        String title = remoteMessage.getData().get("title");
+        if (title != null && title.equals("Remaining day")) {
+            try {
                 sendNotification(remoteMessage);
-            } else {
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else if (remoteMessage.getNotification() != null) {
             Intent pushNotification = new Intent(Constant.PUSH_NOTIFICATION);
-            pushNotification.putExtra("mess" +
-                    "age", remoteMessage.getNotification().getBody());
+
+            pushNotification.putExtra("message", remoteMessage.getNotification().getBody());
             LocalBroadcastManager.getInstance(this).sendBroadcast(pushNotification);
 
         }
     }
 
-    private void sendNotification(RemoteMessage remoteMessage) {
+    private void sendNotification(RemoteMessage remoteMessage) throws UnsupportedEncodingException {
+        int notiID = 0;
+        String message = URLDecoder.decode(remoteMessage.getData().get("body"), "UTF-8");
         Intent intent = new Intent(this, BorrowCartActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
+
 
         Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
         NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
@@ -52,15 +62,17 @@ public class FirebaseNotificationService extends FirebaseMessagingService {
                 .setAutoCancel(true)
                 .setSmallIcon(R.mipmap.ic_launcher_ver2)
                 .setContentTitle(getString(R.string.app_name))
-                .setContentText(remoteMessage.getNotification().getTitle())
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent);
 
+        NotificationCompat.BigTextStyle bigTextStyle = new NotificationCompat.BigTextStyle();
+        bigTextStyle.bigText(message);
+        notificationBuilder.setStyle(bigTextStyle);
         NotificationManager notificationManager =
                 (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+        notificationManager.notify(notiID++ /* ID of notification */, notificationBuilder.build());
 
     }
 }
